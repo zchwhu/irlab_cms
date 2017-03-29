bb1c1e2324e921c0534e16abb754ed89ece1c925
zchwhu <291053953@qq.com>
2017-03-30 00:55:14 +0800

    成员编辑

--- script/memberedit.js
+++ script/memberedit.js
@@ -13,7 +13,7 @@ requirejs.config({
     }
 });
 
-require(['jquery','script/util/sidebar.js','script/util/upload.js','script/util/editor.js','wangEditor'],function ($,sidebar,upload) {
+require(['jquery','script/util/sidebar.js','script/util/upload.js','wangEditor'],function ($,sidebar,upload) {
     $(function () {
         var irSidebar = sidebar('.ir-sidebar',['.ir-main','.ir-header','.ir-footer'],'.icon-menu');
         irSidebar.init(240);

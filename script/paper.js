e54a6a10ea5ebf6aa7ae694eec09c43706dc290d
zch0301 <291053953@qq.com>
2017-03-30 20:30:31 +0800

    论文编辑

--- script/paper.js
+++ script/paper.js
@@ -11,7 +11,7 @@ requirejs.config({
     }
 });
 
-require(['jquery','jquery.modal','script/util/sidebar.js','script/util/reset.js'],function ($,modal,sidebar,reset) {
+require(['jquery','jquery.modal','script/util/sidebar.js','script/util/reset.js','script/util/alert.js','script/util/validate.js'],function ($,modal,sidebar,reset,alert,validate) {
     var irSidebar = sidebar('.ir-sidebar',['.ir-main','.ir-header','.ir-footer'],'.icon-menu');
     irSidebar.init(240);
 
@@ -23,12 +23,70 @@ require(['jquery','jquery.modal','script/util/sidebar.js','script/util/reset.js'
         $(".paper-edit-modal").modal();
     })
 
+    $(document).on("click",".paper-del-btn",function () {
+        var that = $(this);
+        var myAlert = alert({
+            message: '你确定要删除这篇论文条目吗？',
+            type: 'confirm',
+            success: function () {
+                that.parents(".ir-pub-item").remove();
+            }
+        });
+        myAlert.init();
+    })
+
+
     $('.user-add-modal').on('hidden.bs.modal', function (e) {
-        $(this).find(":input").val('');
+        reset('.paper-add-form');
     })
 
     $('.paper-edit-modal').on('hidden.bs.modal', function (e) {
         reset('.paper-edit-form');
     })
 
+    $('#paperEditBtn').click(function () {
+        console.log("ok");
+        var $isValid = validate($(".paper-edit-form"),{
+            title:{
+                validators:{
+                    notEmpty:{
+                        message:"请输入论文引文"
+                    }
+                }
+            },
+            year:{
+                validators:{
+                    notEmpty:{
+                        message:"请输入论文出版时间"
+                    }
+                }
+            },
+            link: {
+                validators:{
+                    notEmpty:{
+                        message:"请输入论文全文地址"
+                    },
+                    isURL: {
+                        message:"链接格式错误，请重新输入"
+                    }
+                }
+            },
+            fullText: {
+                validators: {
+                    notEmpty: {
+                        message: "请上传论文全文"
+                    }
+                }
+            },
+            author: {
+                validators: {
+                    notEmpty: {
+                        message: "请选择论文作者"
+                    }
+                }
+            }
+        })
+
+        return $isValid;
+    })
 })

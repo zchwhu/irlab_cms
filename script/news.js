8640c811a1afb96733826400d4cd89a1d7216f6c
zchwhu <291053953@qq.com>
2017-03-31 02:20:58 +0800

    新闻管理

--- script/news.js
+++ script/news.js
@@ -2,20 +2,25 @@
  * Created by Administrator on 2017/3/28 0028.
  */
 requirejs.config({
-    shim: {
-        'jquery.modal': ['jquery']
-    },
     paths:{
-        'jquery':'lib/jquery-1.7.1.min',
-        'jquery.modal': 'lib/jquery.modal'
+        'jquery':'lib/jquery-1.7.1.min'
     }
 });
 
-require(['jquery','jquery.modal','script/util/sidebar.js'],function ($,modal,sidebar) {
+require(['jquery','script/util/sidebar.js','script/util/alert.js'],function ($,sidebar,alert) {
     var irSidebar = sidebar('.ir-sidebar',['.ir-main','.ir-header','.ir-footer'],'.icon-menu');
     irSidebar.init(240);
 
-    $(".ir-sidebar").click(function () {
-        $(".modal").modal();
+    $(document).on("click",".news-del-btn",function () {
+        var that = $(this);
+        var $target = $(this).parents(".ir-panel");
+        var myAlert = alert({
+            message: '你确定要删除这篇这篇新闻吗？',
+            type: 'confirm',
+            success: function () {
+                $target.remove();
+            }
+        });
+        myAlert.init();
     })
 })

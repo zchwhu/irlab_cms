952d926791e8056e1cce9ea6ea8dac1bbc820c8f
zchwhu <291053953@qq.com>
2017-03-31 02:20:39 +0800

    成员管理

---
+++ script/member.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Administrator on 2017/3/31.
+ */
+requirejs.config({
+    paths:{
+        'jquery':'lib/jquery-1.7.1.min'
+    }
+});
+
+require(['jquery','script/util/sidebar.js','script/util/alert.js'],function ($,sidebar,alert) {
+    var irSidebar = sidebar('.ir-sidebar',['.ir-main','.ir-header','.ir-footer'],'.icon-menu');
+    irSidebar.init(240);
+
+    $(document).on("click",".member-del-btn",function () {
+        var that = $(this);
+        var $target = $(this).parents(".ir-panel");
+        var myAlert = alert({
+            message: '你确定要删除该成员吗？',
+            type: 'confirm',
+            success: function () {
+                $target.remove();
+            }
+        });
+        myAlert.init();
+    })
+})

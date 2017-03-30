ed6d8186fc39f6e2d4d69bfe1589c51aa536856a
zchwhu <291053953@qq.com>
2017-03-31 02:20:29 +0800

    成员编辑

--- script/memberedit.js
+++ script/memberedit.js
@@ -3,17 +3,15 @@
  */
 requirejs.config({
     shim: {
-        'jquery.modal': ['jquery'],
         'wangEditor.min': ['jquery']
     },
     paths:{
         'jquery':'lib/jquery-1.7.1.min',
-        'jquery.modal': 'lib/jquery.modal',
         'wangEditor': 'lib/wangEditor.min'
     }
 });
 
-require(['jquery','script/util/sidebar.js','script/util/upload.js','wangEditor'],function ($,sidebar,upload) {
+require(['jquery','script/util/sidebar.js','script/util/upload.js','script/util/validate.js','wangEditor'],function ($,sidebar,upload,validate) {
     $(function () {
         var irSidebar = sidebar('.ir-sidebar',['.ir-main','.ir-header','.ir-footer'],'.icon-menu');
         irSidebar.init(240);
@@ -50,5 +48,83 @@ require(['jquery','script/util/sidebar.js','script/util/upload.js','wangEditor']
             'fullscreen'
         ];
         editor.create();
+
+        $(".form-confirm-btn").click(function () {
+            var $isValid = validate($("#memberEditForm"),{
+                name:{
+                    validators:{
+                        notEmpty:{
+                            message:"请输入成员姓名"
+                        }
+                    }
+                },
+                title: {
+                    validators:{
+                        notEmpty:{
+                            message:"请输入成员职称"
+                        }
+                    }
+                },
+                email: {
+                    validators:{
+                        notEmpty:{
+                            message:"请输入成员邮箱地址"
+                        },
+                        isEmail:{
+                            message:"邮箱地址不合法，请重新输入"
+                        }
+                    }
+                },
+                direction:{
+                    validators:{
+                        notEmpty:{
+                            message:"请上传成员研究方向"
+                        }
+                    }
+                },
+                avatar: {
+                    validators:{
+                        notEmpty:{
+                            message:"请上传成员头像"
+                        }
+                    }
+                },
+                content: {
+                    validators:{
+                        notEmpty:{
+                            message:"请输入成员详细介绍"
+                        }
+                    }
+                }
+            })
+            var editorValidator = (function () {
+                if(editor.$txt.formatText().trim().length===0){
+                    var $parent = $("textarea[name='content']").parents('.form-group');
+                    if($parent.next(".help-block").length==0){
+                        $("<small class='help-block'>请输入成员详细介绍</small>")
+                            .insertAfter($parent);
+                    }
+                    return false;
+                }
+                return true;
+            })();
+            return $isValid&&editorValidator;
+        })
+
+        // 消除输入框错误提示信息
+        $("input[type='text'],input[type='password'],input[type='number'],textarea,.wangEditor-txt").focus(function () {
+            if($(this).parents(".form-group").next(".help-block").length>0){
+                $(this).parents(".form-group").next(".help-block").remove();
+            }
+        })
+
+        //消除上传图片部分错误提示信息
+        $("input[type='file'],.wangEditor-txt").change(function () {
+            if($(this).val().length>0){
+                if($(this).parents(".form-group").next(".help-block").length>0){
+                    $(this).parents(".form-group").next(".help-block").remove();
+                }
+            }
+        })
     })
 })

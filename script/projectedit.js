6083c88df767be28cfdc08ca9c4f85859c78b2b4
zchwhu <291053953@qq.com>
2017-03-31 02:21:17 +0800

    项目编辑

--- script/projectedit.js
+++ script/projectedit.js
@@ -14,7 +14,7 @@ requirejs.config({
     }
 });
 
-require(['jquery','script/util/sidebar.js','script/util/upload.js','wangEditor','bootstrap-datetimepicker'],function ($,sidebar,upload) {
+require(['jquery','script/util/sidebar.js','script/util/upload.js','script/util/validate.js','wangEditor','bootstrap-datetimepicker'],function ($,sidebar,upload,validate,editor,picker) {
     $(function () {
         var irSidebar = sidebar('.ir-sidebar',['.ir-main','.ir-header','.ir-footer'],'.icon-menu');
         irSidebar.init(240);
@@ -62,5 +62,80 @@ require(['jquery','script/util/sidebar.js','script/util/upload.js','wangEditor',
             'fullscreen'
         ];
         editor.create();
+
+        $(".form-confirm-btn").click(function () {
+            var $isValid = validate($("#projectEditForm"),{
+                title:{
+                    validators:{
+                        notEmpty:{
+                            message:"请输入项目标题"
+                        }
+                    }
+                },
+                member: {
+                    validators:{
+                        notEmpty:{
+                            message:"请输入项目成员"
+                        }
+                    }
+                },
+                startDate:{
+                    validators:{
+                        notEmpty:{
+                            message:"请输入项目开始日期"
+                        }
+                    }
+                },
+                endDate: {
+                    validators:{
+                        notEmpty:{
+                            message:"请输入项目结束日期"
+                        }
+                    }
+                },
+                content: {
+                    validators:{
+                        notEmpty:{
+                            message:"请输入项目介绍"
+                        }
+                    }
+                },
+                headImg: {
+                    validators:{
+                        notEmpty:{
+                            message:"请上传项目图片"
+                        }
+                    }
+                }
+            })
+            var editorValidator = (function () {
+                if(editor.$txt.formatText().trim().length===0){
+                    var $parent = $("textarea[name='content']").parents('.form-group');
+                    if($parent.next(".help-block").length==0){
+                        $("<small class='help-block'>请输入项目介绍</small>")
+                            .insertAfter($parent);
+                    }
+                    return false;
+                }
+                return true;
+            })();
+            return $isValid&&editorValidator;
+        })
+
+        // 消除输入框错误提示信息
+        $("input[type='text'],input[type='password'],input[type='number'],textarea,.wangEditor-txt").focus(function () {
+            if($(this).parents(".form-group").next(".help-block").length>0){
+                $(this).parents(".form-group").next(".help-block").remove();
+            }
+        })
+
+        //消除上传图片部分错误提示信息
+        $("input[type='file'],.wangEditor-txt").change(function () {
+            if($(this).val().length>0){
+                if($(this).parents(".form-group").next(".help-block").length>0){
+                    $(this).parents(".form-group").next(".help-block").remove();
+                }
+            }
+        })
     })
 })
